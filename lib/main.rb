c5185943b05580e31e619c995dceef2a6c1e9914
Mark Billie <mbillie1@gmail.com>
2013-10-29 19:23:57 -0400

    updating stupid file location blah

--- lib/main.rb
+++ lib/main.rb
@@ -13,7 +13,7 @@ class ProblemFiftyFour
 
   def solve
     score = 0
-    file = File.open("/home/mark/poker/poker.txt","r")
+    file = File.open("/Users/Thoughtworker/poker/poker.txt","r")
     while line = file.gets
       hand_one = line.split(" ")[0..4]
       hand_two = line.split(" ")[5..9]
